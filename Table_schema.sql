b58cdcf027976cf76a7bfee12fa3c70544564fad
JoseRoncal7 <50641512+JoseRoncal7@users.noreply.github.com>
2019-09-05 23:23:02 -0400

    Add files via upload

---
+++ Table_schema.sql
@@ -0,0 +1,28 @@
+-- Data Engineering --
+-- Drop Tables if Existing
+DROP TABLE IF EXISTS fifa_ranking;
+DROP TABLE IF EXISTS players;
+
+-- Import CSV Files Into Corresponding SQL Table
+CREATE TABLE "fifa_ranking" (
+    "Country_Rank" VARCHAR   NOT NULL,
+    "Country" VARCHAR   NOT NULL,
+    "Country_abbreviation" VARCHAR NOT NULL,
+    "Confederation" VARCHAR
+    CONSTRAINT "pk_players" PRIMARY KEY (
+        "Country"
+     )
+);
+
+CREATE TABLE "players" (
+    "ID" INT   NOT NULL,
+    "Name" VARCHAR   NOT NULL,
+    "Age" INT  NOT NULL,
+    "Country" VARCHAR   NOT NULL,
+    "Overall" INT NOT NULL,
+    "Club" VARCHAR NOT NULL
+);
+
+-- Adding foreign keys -- 
+ALTER TABLE "players" ADD CONSTRAINT "fk_players_Country" FOREIGN KEY("Country")
+REFERENCES "fifa_ranking" ("Country");
